1ae8778b172014756d72940a7b42c4658c813c30
Aditya Shah <adityashah2701@gmail.com>
2026-08-26 18:46:43 +0530

    Time: 3 ms (72.84%) | Memory: 47.4 MB (35.80%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,51 @@
+class BSTIterator {
+    private Stack<TreeNode> st = new Stack<>();
+    boolean reverse = true;
+
+    public BSTIterator(TreeNode root, boolean isReverse) {
+        reverse = isReverse;
+        pushAll(root);
+    }
+
+    public int next() {
+        TreeNode temp = st.pop();
+        if(reverse == false) pushAll(temp.right);
+        else pushAll(temp.left);
+        return temp.val;
+    }
+
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+
+    private void pushAll(TreeNode node) {
+        while (node != null) {
+            st.push(node);
+            if (reverse == true)
+                node = node.right;
+            else
+                node = node.left;
+        }
+    }
+}
+
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null)
+            return false;
+        BSTIterator l = new BSTIterator(root, false);
+        BSTIterator r = new BSTIterator(root, true);
+        int i = l.next();
+        int j = r.next();
+        while (i < j) {
+            if (i + j == k)
+                return true;
+            else if (i + j < k)
+                i = l.next();
+            else
+                j = r.next();
+        }
+
+        return false;
+    }
+}
