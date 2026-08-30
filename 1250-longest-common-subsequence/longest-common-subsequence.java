6d05910d2ca8aa87a2b4dff4ef31babc8c491e69
Aditya Shah <adityashah2701@gmail.com>
2026-08-30 18:07:32 +0530

    Time: 17 ms (95.82%) | Memory: 42.7 MB (97.84%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,19 +1,37 @@
 class Solution {
-    public int lcs(int i , int j , String s , String t,Integer[][] dp) {
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != null) return dp[i][j];
-        if(s.charAt(i) == t.charAt(j)) {
-            return dp[i][j] = 1 + lcs(i - 1, j - 1 , s , t,dp);
-        } 
+    // public int lcs(int i, int j, String s, String t, Integer[][] dp) {
+    //     if (i == 0 || j == 0)
+    //         return 0;
+    //     if (dp[i][j] != null)
+    //         return dp[i][j];
+    //     if (s.charAt(i - 1) == t.charAt(j - 1)) {
+    //         return dp[i][j] = 1 + lcs(i - 1, j - 1, s, t, dp);
+    //     }
 
-        return dp[i][j] = Math.max(lcs(i - 1,j,s,t,dp),lcs(i,j-1,s,t,dp));
-    }
-    public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        Integer[][] dp = new Integer[n][m];
+    //     return dp[i][j] = Math.max(lcs(i - 1, j, s, t, dp), lcs(i, j - 1, s, t, dp));
+    // }
 
-        
-        return lcs(n-1,m-1,text1,text2,dp);
+    public int longestCommonSubsequence(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[] prev = new int[m + 1];
+        int[] cur = new int[m + 1];
+
+        for (int j = 0; j < m; j++)
+            prev[j] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    cur[j] = 1 + prev[j - 1];
+                } else
+                    cur[j] = Math.max(prev[j], cur[j - 1]);
+            }
+            int[] temp = prev;
+            prev = cur;
+            cur = temp;
+        }
+
+        return prev[m];
     }
 }
