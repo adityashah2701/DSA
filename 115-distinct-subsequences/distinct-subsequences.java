4e2690dcfc135438a8d5e3c9e77aa2179a280696
Aditya Shah <adityashah2701@gmail.com>
2026-08-31 20:18:11 +0530

    Time: 14 ms (92.52%) | Memory: 42.7 MB (95.17%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -20,16 +20,13 @@ class Solution {
         
         dp[0] = 1;
         for (int i = 1; i <= n; i++) {
-            int[] cur = new int[m + 1];
-            cur[0] = 1; 
-            for (int j = 1; j <= m; j++) {
+            for (int j = m; j >= 1; j--) {
                 if (s.charAt(i-1) == t.charAt(j-1)) {
-                     cur[j] = dp[j - 1] + dp[j];
+                     dp[j] = dp[j - 1] + dp[j];
                 } else {
-                     cur[j] = dp[j];
+                     dp[j] = dp[j];
                 }
             }
-            dp = cur;
         }
         return dp[m];
     }
