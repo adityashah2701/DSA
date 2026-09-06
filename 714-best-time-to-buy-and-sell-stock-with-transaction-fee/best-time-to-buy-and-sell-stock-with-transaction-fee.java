c6f2227e96ad7fd3c68cbc87e371c9d9bb36dbd0
Aditya Shah <adityashah2701@gmail.com>
2026-09-06 17:41:13 +0530

    Time: 4 ms (99.60%) | Memory: 65.3 MB (89.01%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,36 +1,56 @@
 class Solution {
-    public int helper(int i, int buy, int[] prices, int fee, int n, Integer dp[][]) {
+    //TOP DOWN
+    // public int helper(int i, int buy, int[] prices, int fee, int n, Integer dp[][]) {
 
-        if (i == n)
-            return 0;
+    //     if (i == n)
+    //         return 0;
 
-        if (dp[i][buy] != null)
-            return dp[i][buy];
+    //     if (dp[i][buy] != null)
+    //         return dp[i][buy];
 
-        int profit;
+    //     int profit;
 
-        if (buy == 1) {
+    //     if (buy == 1) {
 
-            profit = Math.max(
-                -prices[i] + helper(i + 1, 0, prices, fee, n, dp),
-                helper(i + 1, 1, prices, fee, n, dp)
+    //         profit = Math.max(
+    //             -prices[i] + helper(i + 1, 0, prices, fee, n, dp),
+    //             helper(i + 1, 1, prices, fee, n, dp)
+    //         );
+
+    //     } else {
+
+    //         profit = Math.max(
+    //             prices[i] - fee + helper(i + 1, 1, prices, fee, n, dp),
+    //             helper(i + 1, 0, prices, fee, n, dp)
+    //         );
+    //     }
+
+    //     return dp[i][buy] = profit;
+    // }
+    //BOTTOM UP
+    public int maxProfit(int[] prices, int fee) {
+        // int n = prices.length;
+        // Integer dp[][] = new Integer[n][2];
+
+        int buy = 0;
+        int sell = 0;
+
+        for (int i = prices.length - 1; i >= 0; i--) {
+
+            int curBuy = Math.max(
+                -prices[i] + sell,
+                buy
             );
 
-        } else {
-
-            profit = Math.max(
-                prices[i] - fee + helper(i + 1, 1, prices, fee, n, dp),
-                helper(i + 1, 0, prices, fee, n, dp)
+            int curSell = Math.max(
+                prices[i] - fee + buy,
+                sell
             );
+
+            buy = curBuy;
+            sell = curSell;
         }
 
-        return dp[i][buy] = profit;
-    }
-
-    public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        Integer dp[][] = new Integer[n][2];
-
-        return helper(0, 1, prices, fee, n, dp);
+        return buy;
     }
 }
