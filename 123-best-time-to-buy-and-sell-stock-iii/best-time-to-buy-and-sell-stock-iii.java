ac988534a37a84af105fc4493c3d4836e1985188
Aditya Shah <adityashah2701@gmail.com>
2026-09-06 16:33:20 +0530

    Time: 7 ms (51.63%) | Memory: 75.6 MB (88.77%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -22,20 +22,23 @@ class Solution {
         int n = prices.length;
         // Integer[][][] dp = new Integer[n][2][3];
         // return helper(0,1,2,prices,n,dp);
-        int[][][] dp = new int[n+1][2][3];
+        int[][] after = new int[2][3];
+        int[][] cur = new int[2][3];
+
         for(int i = n - 1; i >= 0; i--) {
             for(int buy = 0; buy < 2; buy++) {
                 for(int cap = 1; cap <= 2; cap++){
                      if (buy == 1) {
-                         dp[i][buy][cap] = Math.max(-prices[i] + dp[i + 1][0][cap],dp[i + 1][1][cap]);
+                         cur[buy][cap] = Math.max(-prices[i] + after[0][cap],after[1][cap]);
                     } else {
-                         dp[i][buy][cap] = Math.max(prices[i] + dp[i + 1][1][cap - 1],dp[i + 1][0][cap]);
+                         cur[buy][cap] = Math.max(prices[i] + after[1][cap - 1],after[0][cap]);
                     }
                 }
+                after = cur;
             }
         } 
 
-        return dp[0][1][2];
+        return after[1][2];
 
     }
 }
